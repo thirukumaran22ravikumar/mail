42f9d1f50c74a275db549d9acd21e295c018bcac
thirukumaran22ravikumar <122524280+thirukumaran22ravikumar@users.noreply.github.com>
2024-04-24 17:01:32 +0530

    Update development.rb
    sdd

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -81,15 +81,16 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # ActionMailer::Base.perform_deliveries = true
   # ActionMailer::Base.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: 'gmail.com',
-    user_name: 'thiruhealthguru22@gmail.com',
-    password: 'vlneuklzvkzfghdy',
-    authentication: "plain",
-    enable_starttls_auto: true
-  }
+  
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: 'gmail.com',
+  #   user_name: 'thiruhealthguru22@gmail.com',
+  #   password: 'vlneuklzvkzfghdy',
+  #   authentication: "plain",
+  #   enable_starttls_auto: true
+  # }
 
 
 end
